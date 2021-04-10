054b0f2fcc66cc1958140ec9d8a93b1ead650d49
Costa <clambro.git@gmail.com>
2021-04-10 16:50:58 -0700

    Changed and cleaned strategy

--- src/genetics/population.py
+++ src/genetics/population.py
@@ -43,8 +43,7 @@ class Population:
             self.elites = prev_networks[:NUM_ELITE]
             self.networks = self._spawn_children(prev_networks)
 
-    @staticmethod
-    def _spawn_children(parents):
+    def _spawn_children(self, parents):
         """Generate a list of child networks from a list of parents.
 
         Parameters
@@ -59,7 +58,7 @@ class Population:
         """
         moms = np.random.choice(parents, NETS_PER_POP - NUM_ELITE)
         dads = np.random.choice(parents, NETS_PER_POP - NUM_ELITE)
-        return [NetworkPlayer(mom=m, dad=d) for m, d in zip(moms, dads)]
+        return [NetworkPlayer(gen=self.generation, mom=m, dad=d) for m, d in zip(moms, dads)]
 
     def play_games(self, games, include_elites):
         """Get each network in the population to play a certain number of games.
@@ -83,63 +82,3 @@ class Population:
     def save(self, filename):
         with open(filename, 'wb') as f:
             pickle.dump(self, f)
-
-
-def train_population(final_gen, initial_gen=0, elite=None):
-    """Run a micro-genetic algorithm to evolve a good neural network.
-
-    Each population defaults to 10 networks and plays 50 games. The top 2 from each generation are copied to the next
-    one, but all have the opportunity to reproduce. Every 10 generations, all but the top 2 are killed and 8 new
-    networks are randomly generated to add to the 2 that survived.
-
-    Parameters
-    ----------
-    final_gen : int
-        The total number of generations played is final_gen - initial_gen.  Recommended to be 10*n for positive integer
-        values of n.
-    initial_gen : int
-        The total number of generations played is final_gen - initial_gen
-    elite : List[int]
-        List of networks to copy directly into the new population.
-
-    Returns
-    -------
-    top_scores : List[float]
-        List of floats. The top average score in each generation.
-    best_net : NetworkPlayer
-        The trained networks that performs best.
-    """
-    parents = None
-    top_scores = []
-
-    for gen in range(initial_gen+1, final_gen+1):
-        pop = Population(gen, elite, parents)
-
-        print('Playing games for generation', gen, 'of', final_gen)
-        pop.play_games()
-        pop.sort_by_score()
-        if not pop.generation % 20 and pop.generation != 0:
-            name = 'Generation' + str(pop.generation)
-            np.save(name, pop.genepool[:2])
-
-        top_scores.append(pop.genepool[0].get_avg_score())
-
-        elite = pop.genepool[:2]
-
-        if not gen % 10:
-            parents = None
-        else:
-            parents = pop.genepool
-
-        print('Best network\'s generation =', pop.genepool[0].generation)
-        print('Best network\'s score =', np.rint(pop.genepool[0].get_avg_score()))
-        print('Best network\'s highest tile =', np.rint(pop.genepool[0].get_avg_highest_tile()), '\n')
-
-    plt.figure()
-    plt.title('log_2(Highest Score)')
-    plt.plot(np.log2(top_scores))
-
-    filename = "BestNetGen" + str(final_gen)
-    np.save(filename, pop.genepool[0])
-
-    return top_scores, pop.genepool[0]
---
+++ src/genetics/strategies.py
@@ -0,0 +1,62 @@
+from genetics.population import Population, NETS_PER_POP, NUM_ELITE
+import matplotlib.pyplot as plt
+import numpy as np
+
+
+def run_micro_genetic_alg(num_generations, pop=None):
+    """Run a micro-genetic algorithm to evolve a good neural network.
+
+    Each population defaults to 10 networks and plays 50 games. The top 2 from each generation are copied to the next
+    one, but all have the opportunity to reproduce. Every 10 generations, all but the top 2 are killed and 8 new
+    networks are randomly generated to add to the 2 that survived.
+
+    Parameters
+    ----------
+    final_gen : int
+        The total number of generations played is final_gen - initial_gen.  Recommended to be 10*n for positive integer
+        values of n.
+    initial_gen : int
+        The total number of generations played is final_gen - initial_gen
+    elite : List[int]
+        List of networks to copy directly into the new population.
+
+    Returns
+    -------
+    top_scores : List[float]
+        List of floats. The top average score in each generation.
+    best_net : NetworkPlayer
+        The trained networks that performs best.
+    """
+    top_scores = []
+    for gen in range(num_generations):
+        pop = Population(pop)
+        print(f'Playing games for generation {pop.generation} ({gen + 1} of {num_generations})')
+
+        print('Playing first 30 games.')
+        pop.play_games(30, include_elites=False)
+        pop.networks = pop.get_sorted_networks(include_elites=False)[:NETS_PER_POP // 2 - NUM_ELITE]
+
+        print('Playing next 70 games.')
+        pop.play_games(70, include_elites=False)
+        pop.networks = pop.get_sorted_networks(include_elites=False)[:NETS_PER_POP // 4 - NUM_ELITE]
+
+        print('Playing final 200 games.')
+        pop.play_games(200, include_elites=True)
+
+        if not pop.generation % 10 and pop.generation != 0:
+            pop.save(f'Generation{pop.generation}.pkl')
+
+        top_network = pop.get_sorted_networks(include_elites=True)[0]
+        top_scores.append(top_network.get_avg_score())
+
+        print('Best network\'s generation =', top_network.gen)
+        print('Best network\'s score =', np.rint(top_scores[-1]))
+        print('Best network\'s highest tile =', np.rint(top_network.get_avg_highest_tile()), '\n')
+
+    plt.figure()
+    plt.title('log_2(Highest Score)')
+    plt.plot(np.log2(top_scores))
+
+    pop.save(f'Generation{pop.generation}.pkl')
+
+    return top_scores, top_network
