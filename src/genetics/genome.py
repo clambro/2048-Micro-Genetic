fc86ae2e4381756b6a4d5878456ee94cf236bfa9
Costa <clambro.git@gmail.com>
2021-05-05 19:11:49 -0700

    Simplified strategy

--- src/genetics/genome.py
+++ src/genetics/genome.py
@@ -2,8 +2,8 @@ from game import DIRECTIONS
 import numpy as np
 
 
-HIDDEN_LAYER_SIZE = 1024
-NUM_HIDDEN_LAYERS = 1
+HIDDEN_LAYER_SIZE = 128
+NUM_HIDDEN_LAYERS = 2
 assert NUM_HIDDEN_LAYERS > 0
 
 INPUT_WEIGHT_SHAPE = (16, HIDDEN_LAYER_SIZE)
@@ -99,10 +99,11 @@ class Genome:
         ndarray
             The four direction actions sorted in the order of the network's evaluation.
         """
+        do_activation = np.sign
         x = 3 * (board.reshape(16) / 7 - 1)  # Max tile log-value in 2048 is 14. Normalize to [-3, 3].
-        h = np.sign(x @ self.input_weights)
+        h = do_activation(x @ self.input_weights)
         for w in self.hidden_weights:
-            h = np.sign(h @ w)
+            h = do_activation(h @ w)
         y = h @ self.output_weights  # No non-linearity needed. We only care about order.
         return np.asarray(DIRECTIONS)[y.argsort()[::-1]]
 
